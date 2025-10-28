224b47792915c76a85b2f2efc09c856bb0696a42
nik-hz <nik0001@protonmail.com>
2025-10-28 17:40:37 -0400

    project setup

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ set -e
 ###
 # Installation
 ###
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
-
 
 sh ./shell/install.sh
 sh ./vim/install.sh
--- shell/install.sh
+++ shell/install.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+
+mkdir -p "$HOME/.oh-my-zsh/custom/themes"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k \
+  "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+
+
 echo "🚀 zsh installation"
 ln -sf "${PWD}/shell/.zshrc" "${HOME}/.zshrc"
 
